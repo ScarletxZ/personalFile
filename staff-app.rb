52081e2bba80e977145c6f76846af86a6def2247
Siti Almeida Maryam <horpdorpclop@gmail.com>
2015-03-16 12:36:20 +0800

    Create staff-app.rb

---
+++ staff-app.rb
@@ -0,0 +1,8 @@
+print "What's your first name?"
+first_name = gets.chomp
+print "What's your last name?" 
+last_name = gets.chomp
+print "Position you want to get?"
+position = gets.chomp
+
+print "My first name is {#first_name}, my last name is {#last_name}, and the position is {#position}"
