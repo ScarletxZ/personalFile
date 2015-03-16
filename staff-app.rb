310b754fe7ec1cea6101b0d284bdf8641e6e8501
Siti Almeida Maryam <horpdorpclop@gmail.com>
2015-03-16 17:10:15 +0800

    Update staff-app.rb
    Some mistakes changed

--- staff-app.rb
+++ staff-app.rb
@@ -5,4 +5,4 @@ last_name = gets.chomp
 print "Position you want to get?"
 position = gets.chomp
 
-print "My first name is {#first_name}, my last name is {#last_name}, and the position is {#position}"
+print "My first name is #{first_name}, my last name is #{last_name}, and the position is #{position}"
