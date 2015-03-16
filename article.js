7ee1b07fe3363707378e983904968c64b17f1a64
Siti Almeida Maryam <horpdorpclop@gmail.com>
2015-03-16 11:54:02 +0800

    Create article.js
    Learning purposes.

---
+++ article.js
@@ -0,0 +1,18 @@
+$(function() {
+ var articlename = $('#title').val();
+ var createdate = $('#date').val();
+ var author = $('#author').val();
+ var cont = $('#article').val();
+
+
+/* then, store the data in json file */
+var json = {
+ "title":articlename,
+ "creation":createdate,
+ "authorname":author,
+ "article":cont
+};
+
+/* render it, finally! */
+var data = JSON.stringify(json);
+$('#database').html(data);
