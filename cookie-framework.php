9f9eb63028674ff3dc2dee5f5108ae08eedd7355
Siti Almeida Maryam <horpdorpclop@gmail.com>
2015-03-16 12:05:58 +0800

    Create cookie-framework.php
    For those who want to use.

---
+++ cookie-framework.php
@@ -0,0 +1,22 @@
+<?php
+# Cookie framework for PHP
+# bakeCookie(name,value,time) - create a new cookie
+function bakeCookie(name,value,time) {
+ setcookie(name,value,time+$int());
+}
+
+# readCookie(name) - read a cookie, returns the value of the cookie
+function readCookie(name) {
+ return $_COOKIE[name];
+}
+
+# moreCookie(name,value) - updating cookie
+function moreCookie(name,value) {
+ bakeCookie(name,value);
+ readCookie(name);
+}
+
+# eatCookie(name) - delete cookie
+function eatCookie(name) {
+ unset($_COOKIE[name]);
+}
