e415528b7b03d08ad6d780fc1d51b8a6d698e8f5
Siti Almeida Maryam <horpdorpclop@gmail.com>
2015-04-06 18:30:25 +0800

    Create conditionLoop.js

---
+++ conditionLoop.js
@@ -0,0 +1,26 @@
+/* 
+* Condition Loops
+* This will loop / iterate something you might want if it met certain conditions
+* Example if variable with the name 'cat' equals to 1, then it will
+* iterate / loop the 'cat' variable until it reaches 10
+* The usage for this script will be added below
+*/
+
+/* ================ USAGE ======================
+* To run the function, enter runConditionLoops(); without anything in it.
+* To use the function, enter conditionLoops(); , the params will be added soon. 
+* Another function will be added in the next version 
+*/ 
+
+function runConditionLoops() {
+ return new conditionLoops();
+ if(!window.runConditionLoops) {
+  console.log("You can't use this code without running the core function!");
+ }
+ function conditionLoops(type) {
+  conditional = type;
+  if(conditional === 'variable') {
+   /* code here.. */
+  }
+ }
+}
